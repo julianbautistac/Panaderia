266d59191e0af28a071a27e857e98c0cb6d90c6d
Julian Bautista Cruz <julian.bautista@uabc.edu.mx>
2022-06-12 17:30:51 -0700

    ya funciona correctamente todo el formulario, hace bien los registros

--- database.php
+++ database.php
@@ -3,13 +3,13 @@
 $server = 'localhost';
 $username = 'root';
 
-$password = '';
+$password = 'bautistac5';
 $database = 'panaderia';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password,
   array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); //Nos da los valores en formtato utf8
-
+  //echo 'Se ha conectado a la base de datos';
 } catch (PDOException $e) {
   die('Connection Failed: ' . $e->getMessage());
 }
--- signup.php
+++ signup.php
@@ -1,56 +1,71 @@
+
 <?php
-  require'database.php'; 
-  $message = '';
+require'database.php'; 
+$message = '';
 
-  if (!empty($_POST['usuario']) && !empty($_POST['password'])) {
-    $sql = "INSERT INTO usuarios (usuario, password) VALUES (:usuario, :password)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':usuario', $_POST['usuario']);
-    $stmt->bindParam(':password', $_POST['usuario']);
 
-    if ($stmt->execute()) {
-      $message = 'Se a creado un nuevo usuario';
-    } else {
-      $message = 'No se pudo crear un nuevo usuario';
-    }
+if (!empty($_POST['nombre']) && !empty($_POST['appat'])&& !empty($_POST['apmat']) && !empty($_POST['usuario']) && !empty($_POST['password'])) {
+  $sql = "INSERT INTO usuarios (usuario, password,nombre, apellido_pat,apellido_mat,tipo_usuario) VALUES (:usuario, :password,:nombre,:appat,:apmat,:tipo)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':usuario', $_POST['usuario']);
+  $stmt->bindParam(':password', $_POST['password']);
+  $stmt->bindParam(':nombre', $_POST['nombre']);
+  $stmt->bindParam(':appat', $_POST['appat']);
+  $stmt->bindParam(':apmat', $_POST['apmat']);
+  $stmt->bindParam(':tipo', $_POST['tipo']);
+
+
+
+  if ($stmt->execute()) {
+    $message = 'Se a creado un nuevo usuario';
+  } else {
+    $message = 'No se pudo crear un nuevo usuario';
   }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>SignUp</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <title>SignUp</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
 
-    <?php require 'partials/header.php' ?>
+  <?php require 'partials/header.php' ?>
 
-    <?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
+  <?php if(!empty($message)): ?>
+    <p> <?= $message ?></p>
+  <?php endif; ?>
 
-    <h1>SignUp</h1>
-    <span>or <a href="login.php">Login</a></span>
+  <h1>SignUp</h1>
+  <span>or <a href="login.php">Login</a></span>
 
-    <form action="signup.php" method="POST">
+  <form action="signup.php" method="POST">
 
 
 
-      <input name="usuario" type="text" placeholder="Ingresa un usuario">
-      <input name="password" type="text" placeholder="Enter your Password">
-      <input name="confirm_password" type="password" placeholder="Confirm Password">
-      <select name="Tipo de usuario" id="Tipo">
-      <optgroup label="Tipo">
-        <option value="Administrador">Administrador</option>
-        <option value="Auxiliar">Auxiliar</option>
+    <input name="nombre" type="text" placeholder="nombre">
+    <input name="appat" type="text" placeholder="Apellido paterno">
+    <input name="apmat" type="text" placeholder="Apellido materno">
+    <input name="usuario" type="text" placeholder="Usuario">
+    <input name="password" type="text" placeholder="Enter your Password">
+    <input name="confirm_password" type="password" placeholder="Confirm Password">
+
+
+  
+  <select name="tipo" id="tipo">
+  <optgroup label="tipo de usuario">
+        <option value="1">Administrador</option>
+        <option value="2">Auxiliar</option>
       </optgroup>
       </select>
-      <input type="submit" value="Submit">
-    </form>
+    
+    <input type="submit" value="Registrar">
+  </form>
 
-  </body>
+</body>
 </html>
